44d648ca86083ef12dd8194aa89449a4f22ea335
iwata <77091671+kramkoob-sys@users.noreply.github.com>
2021-07-16 14:42:52 +0900

    Update repository1.java

--- src/repository1.java
+++ src/repository1.java
@@ -2,10 +2,12 @@
 class 
 repository1 {
   public static void main(String args[]){
-  //もり
-  // 0.0.1-iwata2ブランチでの変更
-  // PR制限のテスト
-
-  // 2021/6/15 16:08 feature_0.0.1_iwata2ブランチ
+    string value1 = '1';
+    string value2 = '2';
+    boolean isVlue1;
+    
+    if (value1 == '1') {
+      isValue1 = true;
     }
+  }
 }
